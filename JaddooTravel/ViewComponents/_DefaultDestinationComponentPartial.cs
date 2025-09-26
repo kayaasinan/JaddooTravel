1c7ce46489a0e476e2c5d848040a181d13ed54dd
Sinan Kaya <skaya.sinan@outlook.com>
2025-09-26 21:11:57 +0300

    updated any views

--- JaddooTravel/ViewComponents/_DefaultDestinationComponentPartial.cs
+++ JaddooTravel/ViewComponents/_DefaultDestinationComponentPartial.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JaddooTravel.Services.DestinationServices;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace JaddooTravel.ViewComponents
 {
     public class _DefaultDestinationComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IDestinationService _destinationService;
+
+        public _DefaultDestinationComponentPartial(IDestinationService destinationService)
         {
-            return View();
+            _destinationService = destinationService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _destinationService.GetAllDestinationAsync();
+            return View(values);
         }
     }
 }
